e3126d339d0cb743e1a73a503609361f4a167812
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-09-23 21:47:52 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Content from './content';
+
+import "./index.css";
+
+const App = () => <Content />;
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/content.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Content() {
+    return <div>
+    <h1>HOST</h1>
+    Hi there, I'm React from Webpack 5.</div>
+}
