6a00cb94878d6ac81e4fad12580ed95a2a3d3dd7
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-09-25 00:40:26 +0800

    new changes

--- src/content.jsx
+++ src/content.jsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 export default function Content() {
   return (
     <div>
-      <h1>Mini App v2</h1>
-      <p>Cutting edge feature</p>
+      <h1>Mini App v3</h1>
+      <p>Bleeding edge feature</p>
     </div>
   );
 }
