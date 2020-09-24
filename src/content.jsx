dc67b02f569aaac77dcf6397508994e981de17de
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-09-25 00:52:54 +0800

    add our best friend lodash

--- src/content.jsx
+++ src/content.jsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
+import _ from 'lodash';
 
 export default function Content() {
+  const [randomString] = React.useState(() =>
+    _.times(20, () => _.random(35).toString(36)).join('')
+  );
+
   return (
     <div>
       <h1>Mini App v3</h1>
       <p>Bleeding edge feature</p>
+      <p>{randomString}</p>
     </div>
   );
 }
