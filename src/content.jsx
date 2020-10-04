8fb66443499a5be434c31736b7c446b69828a276
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-10-04 23:53:19 +0800

    add change

--- src/content.jsx
+++ src/content.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function Content() {
   return (
     <div>
-      <h1>Mini App</h1>
+      <h1>Mini App in React KL Oct</h1>
       <p>I am mini app yo</p>
     </div>
   );
