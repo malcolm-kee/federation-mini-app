201c9ecbe30602559f841e758f8613c2109d8825
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-10-06 02:11:11 +0800

    update to v4

--- src/content.jsx
+++ src/content.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function Content() {
   return (
     <div>
-      <h1>Mini App v3</h1>
+      <h1>Mini App v4</h1>
       <p>Cutting edge feature</p>
     </div>
   );
