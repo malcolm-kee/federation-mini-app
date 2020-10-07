3ed7ae6fefbd04d46ce531c108ff5053783cf8ec
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-10-07 21:24:30 +0800

    change

--- src/content.jsx
+++ src/content.jsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 export default function Content() {
   return (
     <div>
-      <h1>Mini App v4</h1>
-      <p>Cutting edge feature</p>
+      <h1>Mini App v5</h1>
+      <p>Malcolm just embarassed himself by not sharing his screen.</p>
     </div>
   );
 }
