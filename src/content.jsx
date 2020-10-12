43b4b125a4257d08adc9934d92ee274460a06511
Malcolm Kee <malcolm.kee@setel.my>
2020-10-12 14:11:32 +0800

    bump webpack to v5

--- src/content.jsx
+++ src/content.jsx
@@ -3,8 +3,9 @@ import * as React from 'react';
 export default function Content() {
   return (
     <div>
-      <h1>Mini App v5</h1>
+      <h1>Mini App v6</h1>
       <p>Malcolm just embarassed himself by not sharing his screen.</p>
+      <p>Finally webpack 5 is final.</p>
     </div>
   );
 }
