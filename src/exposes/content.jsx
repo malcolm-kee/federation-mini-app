c21e5891cf6214e1d638d070495c5e4039600bf3
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-03-15 19:57:56 +0800

    update content

--- src/exposes/content.jsx
+++ src/exposes/content.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function Content() {
   return (
     <div>
-      <h1>Mini App</h1>
+      <h1>Mini App demo in MoneyLion</h1>
       <p>This is an important content in Mini App.</p>
     </div>
   );
